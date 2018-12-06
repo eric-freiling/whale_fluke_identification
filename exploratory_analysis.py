e7cab8ce440c47eb4f62d507d4a3188c614292a9
eric-freiling <efreiling@gmail.com>
2018-12-06 07:43:51 -0800

    update

--- exploratory_analysis.py
+++ exploratory_analysis.py
@@ -1,5 +1,15 @@
 import numpy as np
-print("Start")
-x = np.array([0, 1, 2, 3, 4])
-print(x)
-print("Done")
+import pandas as pd
+from os.path import join, exists
+from os import makedirs, listdir
+import cv2
+
+
+data_path = "../data/whale_fluke_data"
+train_path = join(data_path, "train")
+test_path = join(data_path, "test")
+train_csv_path = join(data_path, "train.csv")
+
+train_dir_names = listdir(train_path)
+label_data = pd.read_csv(train_csv_path)
+labels, counts = np.unique(label_data.Id, return_counts=True)
