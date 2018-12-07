aa02dc31841336d9f3525139455f69a984bcc8d1
eric-freiling <39280972+eric-freiling@users.noreply.github.com>
2018-12-07 15:00:04 -0800

    testing work and personal git

--- exploratory_analysis.py
+++ exploratory_analysis.py
@@ -13,3 +13,25 @@ train_csv_path = join(data_path, "train.csv")
 train_dir_names = listdir(train_path)
 label_data = pd.read_csv(train_csv_path)
 labels, counts = np.unique(label_data.Id, return_counts=True)
+
+file_names = label_data.Image.values
+file_ids = label_data.Id.values
+
+
+def get_im_size_array():
+    im_size_array = []
+    for name in file_names:
+        temp_im = cv2.imread(join(train_path, name))
+        im_size_array.append(temp_im.shape)
+
+    return im_size_array
+
+
+file_name = df.values[0, 0]
+file_id = df.values[0, 1]
+im = cv2.imread(join(train_path, file_name))
+
+# loc = np.where(file_ids == file_id)[0]
+# ims = []
+# for idx in loc:
+#    ims.append(cv2.imread(join(train_path, file_names[idx])))
