fc6b18b697c4be852e54ea8a5091ec9f999ccd01
eric-freiling <efreiling@gmail.com>
2018-12-30 10:03:36 -0800

    update parameters

--- parameters.py
+++ parameters.py
@@ -1,6 +1,7 @@
 from os.path import join
+from pathlib import Path
 
-freeze_flag = True
+freeze_flag = False
 input_shape = (224, 224, 3)
 bw_flag = False
 filter_sizes = [64, 128, 128, 256]
@@ -14,8 +15,8 @@ dense_acts = ["sigmoid", "relu"]
 # Set parameters
 validation_flag = True
 validation_percent = 0.20
-batch_size = 10
-epochs = 200
+batch_size = 20
+epochs = 10000
 learning_rate = 0.0001
 print_iter = 10
 save_iter = 10
@@ -26,3 +27,7 @@ if bw_flag:
 else:
     model_name = str(input_shape[0]) + "_" + str(input_shape[1]) + '_color'
 model_path = join(save_dir, model_name)
+
+data_path = Path("../data/whale_fluke_data")
+train_path = Path(data_path / "train")
+test_path = Path(data_path / "test")
